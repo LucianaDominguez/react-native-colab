c413d8d20ab015279896771bb9f8d5742414dcba
Android Studio <devandroid@ort>
2025-08-11 14:05:47 -0300

    feat: profile card component

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ProfileCard from './src/components/ProfileCard';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ProfileCard name={"Shawn Mendes"} username={"shawn.mendes@gmail.com"} avatarUrl={"https://static.wikia.nocookie.net/copcal/images/3/37/Shawnwikia.jpg/revision/latest?cb=201803030547280"} bio={"Singer"}></ProfileCard>
       <StatusBar style="auto" />
     </View>
   );
